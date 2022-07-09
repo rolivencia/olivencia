f3c21228a9d821fff87d77b846141d9c36b1cee9
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2022-07-09 09:35:35 -0300

    Commented out header and remove period in title.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,23 +3,23 @@ import React from "react"
 export default function Home() {
   return (
     <div class="container">
-      <div class="nav horizontal-align">
-        <div class="nav-item">
-          <a href="#about">Sobre Nosotros</a>
-        </div>
-        <div class="nav-item">
-          <a href="#services">Servicios</a>
-        </div>
-        <div class="nav-item">
-          <a href="#team">Nuestro Equipo</a>
-        </div>
-        <div class="nav-item">
-          <a href="#projects">Proyectos</a>
-        </div>
-      </div>
+      {/*<div class="nav horizontal-align">*/}
+      {/*  <div class="nav-item">*/}
+      {/*    <a href="#about">Sobre Nosotros</a>*/}
+      {/*  </div>*/}
+      {/*  <div class="nav-item">*/}
+      {/*    <a href="#services">Servicios</a>*/}
+      {/*  </div>*/}
+      {/*  <div class="nav-item">*/}
+      {/*    <a href="#team">Nuestro Equipo</a>*/}
+      {/*  </div>*/}
+      {/*  <div class="nav-item">*/}
+      {/*    <a href="#projects">Proyectos</a>*/}
+      {/*  </div>*/}
+      {/*</div>*/}
       <div class="center">
         {/*<h1>Olivencia C&D</h1>*/}
-        <h1>Sitio en construcción.</h1>
+        <h1>Sitio en construcción</h1>
       </div>
     </div>
   )
