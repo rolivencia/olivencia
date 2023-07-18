6df65d94625bb6e2e000dab8a1de68fe9b5e11f0
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-07-18 19:59:16 -0300

    [#10] - Add picture and name

--- apps/olivencia/src/app/pages/(home).page.ts
+++ apps/olivencia/src/app/pages/(home).page.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { NgForOf } from '@angular/common';
+import { NgForOf, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'olivencia-home',
   standalone: true,
   template: `
     <main class="h-screen">
-      <article class="h-full grid grid-cols-1 md:grid-cols-[1fr_680px_1fr]">
-        <section class="md:col-start-2 md:col-end-2">
+      <article class="grid grid-cols-1 md:grid-cols-[1fr_680px_1fr] grid-rows-2">
+        <div class="col-start-2 col-end-3 row-start-1 row-end-2 flex flex-col items-center">
+          <img
+            class="rounded-full"
+            height=128
+            width=128
+            [src]="pictureUrl" alt="Foto frontal de Ramiro Olivencia"
+          />
+          <h1>Ramiro Olivencia</h1>
+        </div>
+        <section class="md:col-start-2 md:col-end-2 row-start-2 row-end-3">
           <div class="grid grid-col-1 gap-6">
             <ng-container *ngFor="let link of links">
               <div class="h-8 border-2">
@@ -19,10 +28,12 @@ import { NgForOf } from '@angular/common';
       </article>
     </main>`,
   imports: [
-    NgForOf
+    NgForOf,
+    NgOptimizedImage
   ]
 })
 export default class HomeComponent {
+  readonly pictureUrl: string = 'https://avatars.githubusercontent.com/u/32349705?v=4'
   readonly links: Link[] = [
     {
       label: 'Discord',
