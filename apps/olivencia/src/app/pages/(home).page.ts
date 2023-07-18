188792f5c29e53cbb3c893a600acfeb13d046b9c
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2023-07-18 19:13:45 -0300

    [#10] - Remove references to welcome component. Adjust columns width

--- apps/olivencia/src/app/pages/(home).page.ts
+++ apps/olivencia/src/app/pages/(home).page.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 
-import { AnalogWelcomeComponent } from './analog-welcome.component';
-
 @Component({
   selector: 'olivencia-home',
   standalone: true,
-  // imports: [AnalogWelcomeComponent],
   template: `
     <main class="h-screen">
-      <section class="h-full grid grid-cols-1 md:grid-cols-3">
+      <section class="h-full grid grid-cols-1 md:grid-cols-[1fr_680px_1fr]">
         <h1 class="md:col-start-2 md:col-end-2">Página en Construcción</h1>
       </section>
     </main>`,
