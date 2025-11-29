eec74af7058042d822060bc65007b1d104e2f91c
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-11-29 08:59:36 -0300

    Add Analytics provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,36 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { environment } from '../environments/environment';
+import { Analytics } from './providers/analytics';
+
+function initializeAnalytics() {
+  return async () => {
+    if (!environment.production) {
+      return;
+    }
+
+    const analytics = inject(Analytics);
+    await analytics.init();
+  };
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+
+    // Initializers
+    provideAppInitializer(initializeAnalytics()),
+  ],
 };
---
+++ src/app/providers/analytics.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Analytics {
+  async init() {
+    if (!environment.clarityProjectId) {
+      return;
+    }
+
+    try {
+      const clarityModule = await import('@microsoft/clarity');
+      const clarity = clarityModule.default;
+      clarity.init(environment.clarityProjectId);
+    } catch (error) {
+      console.error('Failed to initialize Clarity:', error);
+    }
+  }
+}
